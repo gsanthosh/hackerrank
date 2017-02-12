cf5c60dcd61fa73730f00a226ea19a334b74ee8a
Santhosh Gnanasurian <santhosh.gnanasurian@non.schneider-electric.com>
2017-02-12 11:03:39 +0530

    Bitwise and code

---
+++ src/main/java/com/santhosh/hackerrank/algorithm/thirtydaychallenge/BitwiseAnd.java
@@ -0,0 +1,68 @@
+package com.santhosh.hackerrank.algorithm.thirtydaychallenge;
+
+import java.util.Scanner;
+
+/*
+Objective 
+Welcome to the last day! Today, we're discussing bitwise operations. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given set . Find two integers,  and  (where ), from set  such that the value of  is the maximum possible and also less than a given integer, . In this case,  represents the bitwise AND operator.
+
+Input Format
+
+The first line contains an integer, , the number of test cases. 
+Each of the  subsequent lines defines a test case as  space-separated integers,  and , respectively.
+
+Constraints
+
+Output Format
+
+For each test case, print the maximum possible value of  on a new line.
+
+Sample Input
+
+3
+5 2
+8 5
+2 2
+Sample Output
+
+1
+4
+0
+Explanation
+
+ 
+
+All possible values of  and  are:
+
+The maximum possible value of  that is also  is , so we print  on a new line.
+*/
+public class BitwiseAnd {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			int n = in.nextInt();
+			int k = in.nextInt();
+			int[] set = new int[n];
+			int maxNumber = 0;
+			for (int i = 1; i <= n; i++) {
+				set[i - 1] = i;
+			}
+			for (int j = 1; j <= n - 1; j++) {
+				for (int l = j + 1; l <= n; l++) {
+					int andResult = set[j - 1] & set[l - 1];
+					if (andResult > maxNumber && andResult < k) {
+						maxNumber = andResult;
+					}
+				}
+			}
+
+			System.out.println(maxNumber);
+		}
+	}
+
+}
