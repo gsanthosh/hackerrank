0e5edc75716440e7c70d0575781c44089bdbc88b
santhosh <santhosh@192.168.1.16>
2017-12-01 07:47:46 +0530

    Check in heap datastructure

---
+++ src/main/java/com/santhosh/hackerrank/algorithm/datastructure/Heap.java
@@ -0,0 +1,162 @@
+package com.santhosh.hackerrank.algorithm.datastructure;
+
+import java.util.ArrayList;
+/**
+ * Implementation of the heap.
+ * @author santhosh
+ *
+ */
+public class Heap {
+	private final ArrayList<Integer> heapElements=new ArrayList<>();
+    
+	/**
+	 * Get parent.Parent is element at i-1/2
+	 */
+	public int getParent(int currentPosition) {
+		if(currentPosition>heapElements.size()-1) {
+			throw new IllegalArgumentException("The child pos is greater than heap size");
+		}
+		return (currentPosition-1)/2;
+	}
+	
+	/**
+	 * Get left child
+	 */
+	public int getLeftChild(int parentPosition) {
+		if(parentPosition>heapElements.size()-1) {
+			throw new IllegalArgumentException("The parent pos is greater than heap size");
+		}
+		int leftChildPosition=(2*parentPosition)+1;
+		if(leftChildPosition>heapElements.size()-1) {
+			throw new IllegalStateException("Left chlid doesnt exists");
+		}
+		return heapElements.get(leftChildPosition);
+	}
+	
+	/**
+	 * Get left child
+	 */
+	public boolean doesLeftChildExists(int parentPosition) {
+		int leftChildPosition=(2*parentPosition)+1;
+		if(leftChildPosition>heapElements.size()-1) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Get right child
+	 */
+	public int getRightChild(int parentPosition) {
+		if(parentPosition>heapElements.size()-1) {
+			throw new IllegalArgumentException("The parent pos is greater than heap size");
+		}
+		int rightChildPosition=(2*parentPosition)+2;
+		if(rightChildPosition>heapElements.size()-1) {
+			throw new IllegalStateException("Left chlid doesnt exists");
+		}
+		return heapElements.get((2*parentPosition)+2);
+	}
+	
+	public boolean doesRightChildExists(int parentPosition) {
+		int rightChildPosition=(2*parentPosition)+2;
+		if(rightChildPosition>heapElements.size()-1) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Get top element
+	 */
+	public int getTopElement() {
+		if(heapElements.size()==0) {
+			throw new IllegalStateException();
+		}
+		return heapElements.get(0);
+	}
+	
+	/**
+	 * Remove top element
+	 */
+	public int removeTopElement() {
+		if(heapElements.size()==0) {
+			throw new IllegalStateException();
+		}
+		return heapElements.remove(0);
+	}
+	
+	public boolean isEmpty() {
+		return heapElements.isEmpty();
+	}
+	
+	/**
+	 * Max heapify
+	 */
+	public void maxHeapify() {
+		int length=heapElements.size();
+		for(int i=0;i<length-2;i++) {
+			int parentValue=heapElements.get(i);
+			if(doesLeftChildExists(i) && parentValue <getLeftChild(i)) {
+				int tmp=getLeftChild(i);
+				heapElements.set((2*i)+1, parentValue);
+				heapElements.set(i, tmp);
+				i=-1;
+				continue;
+			}
+			if(doesRightChildExists(i) && parentValue <getRightChild(i)) {
+				int tmp=getRightChild(i);
+				heapElements.set((2*i)+2, parentValue);
+				heapElements.set(i, tmp);
+				i=-1;
+				continue;
+			}
+		}
+	}
+	
+	/**
+	 * in heapify
+	 */
+	public void minHeapify() {
+		int length=heapElements.size();
+		for(int i=0;i<length-2;i++) {
+			int parentValue=heapElements.get(i);
+			if(doesLeftChildExists(i) && parentValue >getLeftChild(i)) {
+				int tmp=getLeftChild(i);
+				heapElements.set((2*i)+1, parentValue);
+				heapElements.set(i, tmp);
+				i=-1;
+				continue;
+			}
+			if(doesRightChildExists(i) && parentValue >getRightChild(i)) {
+				int tmp=getRightChild(i);
+				heapElements.set((2*i)+2, parentValue);
+				heapElements.set(i, tmp);
+				i=-1;
+				continue;
+			}
+		}
+	}
+	
+	
+	public void addElementToHeap(int i) {
+		heapElements.add(i);
+	}
+	
+	public static void main(String[] args) {
+		Heap heap=new Heap();
+		//int[] inputArr= {23,22,15,2,57,53,2,57,2,45,90,45,32,67,21,57,9,43};
+		int[] inputArr= {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<inputArr.length;i++) {
+			heap.addElementToHeap(inputArr[i]);
+		}
+		for(int i=0;i<inputArr.length;i++) {
+			heap.minHeapify();
+			System.out.println(heap.getTopElement());
+			heap.removeTopElement();
+		}
+				
+	}
+	
+}
+
